44bb70269d9790e29d2ff569efd0740649a9244c
ruthwekesa <wekesaruth569@gmail.com>
2022-08-22 12:35:13 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const findMatching = (drivers, str) => {
+    const filterArr = drivers.filter((filtered) => {
+        return filtered.toUpperCase() === str.toUpperCase();
+
+    });
+    return filterArr;
+}
+
+const fuzzyMatch = (drivers, str) => {
+    const filterArr = drivers.filter((filtered) => {
+        return filtered[0] === str[0];
+    });
+    return filterArr;
+}
+
+const matchName = (drivers, str) => {
+    const filterArr = drivers.filter((filtered) => {
+        console.log(filtered.name);
+        return filtered.name === str;
+    });
+    return filterArr;
+}
